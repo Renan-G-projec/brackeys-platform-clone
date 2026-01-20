8bb0a55b6515bed29c6889853facb0010cdee23c
Renan-G-projec <renanfartes@gmail.com>
2026-01-20 12:45:24 -0300

    Creating empty window

--- include/game.hpp
+++ include/game.hpp
@@ -1,8 +1,20 @@
 // Ad Maiorem Dei Gloriam!
+#pragma once
+
+#include "SFML/Graphics.hpp"
 
 class Game {
 public:
-    Game() {};
+    Game();
 
-    void run() {};
+    void run();
+
+private:
+    // Constants
+    static constexpr unsigned int WWIDTH = 1280;
+    static constexpr unsigned int WHEIGHT = 720;
+    static constexpr unsigned int TARGETFPS = 60;
+
+    sf::RenderWindow window;
+    sf::Event event;
 };
--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,27 @@
 // Ad Maiorem Dei Gloriam!
+#include "../include/game.hpp"
+
+Game::Game() {}
+
+void Game::run() {
+
+    window.create({WWIDTH, WHEIGHT}, "Brackeys Platform Game - Clone");
+
+    window.setFramerateLimit(TARGETFPS);
+
+    // Game Loop
+    while (window.isOpen()) {
+        while(window.pollEvent(event)) {
+
+            // Exiting
+            if (event.type == event.Closed) {
+                window.close();
+                break;
+            }
+
+        }
+
+        window.clear();
+        window.display();
+    }
+}
